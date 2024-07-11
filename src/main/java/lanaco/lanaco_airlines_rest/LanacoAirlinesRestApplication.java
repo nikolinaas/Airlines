0fe12aba8bcf539800996658c5f452069b502975
Nikolina Stojčić <stojcicn00@gmail.com>
2024-07-11 18:25:43 +0200

    initial commit

---
+++ src/main/java/lanaco/lanaco_airlines_rest/LanacoAirlinesRestApplication.java
@@ -0,0 +1,13 @@
+package lanaco.lanaco_airlines_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanacoAirlinesRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanacoAirlinesRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lanaco/lanaco_airlines_rest/LanacoAirlinesRestApplicationTests.java
@@ -0,0 +1,13 @@
+package lanaco.lanaco_airlines_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanacoAirlinesRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
